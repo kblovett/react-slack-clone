f1bc2e345f6272837ef4c61b06dd08c8c5c113b3
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-07-21 12:09:10 -0600

    fourth commit

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,8 +1,79 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon,
+} from 'semantic-ui-react';
 
 class Register extends React.Component {
+  state = {};
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event });
+  };
+
   render() {
-    return <div>Register</div>;
+    return (
+      <Grid textAlign='center' verticalAlign='middle' className='app'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h2' icon color='orange' textAlign='center'>
+            <Icon name='puzzle piece' color='orange' />
+            Register for DevChat
+          </Header>
+          <Form size='large'>
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name='username'
+                icon='user'
+                iconPosition='left'
+                placeholder='Username'
+                onChange={this.handleChange}
+                type='text'
+              />
+              <Form.Input
+                fluid
+                name='email'
+                icon='mail'
+                iconPosition='left'
+                placeholder='Email Address'
+                onChange={this.handleChange}
+                type='email'
+              />
+              <Form.Input
+                fluid
+                name='password'
+                icon='lock'
+                iconPosition='left'
+                placeholder='Password'
+                onChange={this.handleChange}
+                type='password'
+              />
+              <Form.Input
+                fluid
+                name='passwordConfirmation'
+                icon='repeat'
+                iconPosition='left'
+                placeholder='Confirm Password'
+                onChange={this.handleChange}
+                type='password'
+              />
+              <Button fluid size='large' color='orange'>
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            Already registered? <Link to='/login'>Login here.</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
