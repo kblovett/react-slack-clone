c5a2c20f0b05f756a841dfc822c7a1cee5066abb
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-07-21 10:13:08 -0600

    second commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 const Root = () => (
   <Router>
     <Switch>
-      <Route exact path='/' component={App} />
+      <Route path='/' component={App} />
       <Route path='/login' component={Login} />
       <Route path='/register' component={Register} />
     </Switch>
