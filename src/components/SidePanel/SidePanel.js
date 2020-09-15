2f76cabe94983c6e07f260ced550885d24577240
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-09-14 20:50:47 -0600

    7.20 Add ChannelPanel component

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Menu } from 'semantic-ui-react';
 
 import UserPanel from './UserPanel';
+import ChannelPanel from './ChannelPanel';
 
 class SidePanel extends React.Component {
   render() {
@@ -15,6 +16,7 @@ class SidePanel extends React.Component {
         style={{ background: '#4c3c4c', fontSize: '1.2rem' }}
       >
         <UserPanel currentUser={currentUser} />
+        <ChannelPanel />
       </Menu>
     );
   }
