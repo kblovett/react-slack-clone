3d59519fa77716f5a86b88922eeeba7a679e9e49
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-09-15 23:39:02 -0600

    8.28-29 Create Messages collection, clear messages field, disable send button on loading

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import firebase from '../../firebase';
 import { Segment, Comment } from 'semantic-ui-react';
 
 import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
 
 class Messages extends React.Component {
+  state = {
+    messagesRef: firebase.database().ref('messages'),
+    channel: this.props.currentChannel,
+    user: this.props.currentUser,
+  };
   render() {
+    const { messagesRef, channel, user } = this.state;
     return (
       <React.Fragment>
         <MessagesHeader />
@@ -14,7 +21,11 @@ class Messages extends React.Component {
           <Comment.Group className='messages'>{/* Messages */}</Comment.Group>
         </Segment>
 
-        <MessageForm />
+        <MessageForm
+          messagesRef={messagesRef}
+          currentChannel={channel}
+          currentUser={user}
+        />
       </React.Fragment>
     );
   }
