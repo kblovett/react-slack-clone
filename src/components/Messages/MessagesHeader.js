12a517f29f8cc69de356b99e4d9060bb00bbf737
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-09-18 22:55:11 -0600

    10.38 Create DirectMessages component

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -15,8 +15,7 @@ class MessagesHeader extends React.Component {
         {/* Channel Title */}
         <Header fluid='true' as='h2' floated='left' style={{ marginBottom: 0 }}>
           <span>
-            {channelName}
-            <Icon name={'star outline'} color='black' />
+            {channelName} <Icon name={'star outline'} color='black' />
           </span>
           <Header.Subheader>{numUniqueUsers}</Header.Subheader>
         </Header>
