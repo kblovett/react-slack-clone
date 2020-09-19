f6ff5f1c5a4eb6b650b8dd855595a68011828c1c
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-09-18 20:40:51 -0600

    9.34 ProgressBar component

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -3,6 +3,7 @@ import uuidv4 from 'uuid/v4';
 import firebase from '../../firebase';
 
 import FileModal from './FileModal';
+import ProgressBar from './ProgressBar';
 
 import { Segment, Button, Input } from 'semantic-ui-react';
 
@@ -131,7 +132,14 @@ class MessageForm extends React.Component {
   closeModal = () => this.setState({ modal: false });
 
   render() {
-    const { errors, message, loading, modal } = this.state;
+    const {
+      errors,
+      message,
+      loading,
+      modal,
+      uploadState,
+      percentUploaded,
+    } = this.state;
     return (
       <Segment className='message__form'>
         <Input
@@ -165,12 +173,16 @@ class MessageForm extends React.Component {
             labelPosition='right'
             icon='cloud upload'
           />
-          <FileModal
-            modal={modal}
-            uploadFile={this.uploadFile}
-            closeModal={this.closeModal}
-          />
         </Button.Group>
+        <FileModal
+          modal={modal}
+          uploadFile={this.uploadFile}
+          closeModal={this.closeModal}
+        />
+        <ProgressBar
+          uploadState={uploadState}
+          percentUploaded={percentUploaded}
+        />
       </Segment>
     );
   }
