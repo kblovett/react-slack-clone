5322c011bca3c3608424c2d6833953f2ca8a6780
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-09-20 15:36:40 -0600

    11.43 Add Notifications to public channels

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Menu } from 'semantic-ui-react';
 
 import UserPanel from './UserPanel';
+import Starred from './Starred';
 import Channels from './Channels';
 import DirectMessages from './DirectMessages';
 
@@ -19,6 +20,7 @@ class SidePanel extends React.Component {
         style={{ background: '#4c3c4c', fontSize: '1.2rem' }}
       >
         <UserPanel currentUser={currentUser} />
+        <Starred />
         <Channels currentUser={currentUser} />
         <DirectMessages currentUser={currentUser} />
       </Menu>
