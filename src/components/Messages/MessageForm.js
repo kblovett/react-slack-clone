88e84a0bc7ca7fc3a5f0654d3b7e65c12fdc3b69
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-09-25 01:19:12 -0600

    17.62 Add enterKey shortcut for sending messages

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -162,7 +162,11 @@ class MessageForm extends React.Component {
     );
   };
 
-  handleKeyDown = () => {
+  handleKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      this.sendMessage();
+    }
+
     const { message, typingRef, channel, user } = this.state;
 
     if (message) {
